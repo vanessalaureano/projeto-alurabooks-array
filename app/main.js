40b254f81ef762e13bb8cf13966fda0009de5e42
vanessalaureano <solytagamer@gmail.com>
2023-07-10 03:13:45 -0300

    update

--- app/main.js
+++ app/main.js
@@ -3,26 +3,10 @@ const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.js
 
 getBuscarLivrosDaAPI();
 
-const elementoParaInserirLivros = document.getElementById('livros');
-
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI); // espere por uma promessa
     livros = await res.json();
-    console.table(livros)
-    exibirLivrosNaTela(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirLivrosNaTela(livrosComDesconto)
 }
 
-function exibirLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">${livro.titulo}</h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
-        <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-        `
-    });
-}
