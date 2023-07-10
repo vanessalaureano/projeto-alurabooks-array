819d6ff9fb1f19a74b60cf9c394e5d8f5e1d8c83
vanessalaureano <solytagamer@gmail.com>
2023-07-10 05:00:57 -0300

    update

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,12 +1,30 @@
-const botoes = document.querySelectorAll('.btn');
-botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
-
-/* const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
-btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivros); */
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
-    const elementoBtn = document.getElementById(this.id);
-    const categoria = elementoBtn.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
-    exibirLivrosNaTela(livrosFiltrados);
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+
+    if(categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
+    }
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
 }
--- app/metodoMap.js
+++ app/metodoMap.js
@@ -1,8 +1,7 @@
 function aplicarDesconto(livros) {
-    const desconto = 0.3;
+    const desconto = 0.3
     livrosComDesconto = livros.map(livro => {
         return {...livro, preco: livro.preco - (livro.preco * desconto)}
     })
     return livrosComDesconto
-    
 }
